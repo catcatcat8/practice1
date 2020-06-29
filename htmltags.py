8d7ad130e298e9c4160348c03f8b4916f22394c6
catcatcat8 <38365386+catcatcat8@users.noreply.github.com>
2020-06-29 16:21:08 +0300

    Список html тегов документа
    Проверка существования файла через цикл со списком корневых папок

--- htmltags.py
+++ htmltags.py
@@ -1,18 +1,18 @@
-# e3rewrertfregtr
+# Лебедев Евгений Получение списка html тегов документа
 import os
 
-_sl = ["practice1", "Git"]
+_sl = ["practice1", "Git"]     #Список корневых папок
 t = os.getcwd()
-_rF =None
+_rF = None
 for item in _sl:
-    _wd = f'{t[:t.rfind(item)+len(item)]}\\HTML'
-    if os.path.isfile(f'{_wd}\\index.html'):
-        _fr = _wd1
+    _wd = f'{t[:t.rfind(item)+len(item)]}\\HTML'     
+    if os.path.isfile(f'{_wd}\\index.html'):   #Проверка существования файла
+        _rF = _wd
         break
 
 if _rF is not None:
     tags = []
-    with open (_rF) as f:
+    with open (f'{_rF}\\index.html') as f:
         for line1 in range(1):
             next(f)
         for line in f:
