2fbd27afd1970196f661f7aaeebd9aa5b35d7bcd
MirBoss <mirboss@gmail.com>
2020-06-30 07:09:59 +0300

    Переформатирование кода под модель функционального программирования
    Добавлено:
     - файл readme (пояснительный материал)
     - .gitignore (список файлов не входящих для хранения в репозитории)
     - графические материалы

--- htmltags.py
+++ htmltags.py
@@ -1,29 +1,43 @@
 # Лебедев Евгений Получение списка html тегов документа
 import os
 
-_sl = ["practice1", "Git"]     #Список корневых папок
-t = os.getcwd()
-_rF = None
-for item in _sl:
-    _wd = f'{t[:t.rfind(item)+len(item)]}\\HTML'     
-    if os.path.isfile(f'{_wd}\\index.html'):   #Проверка существования файла
-        _rF = _wd
-        break
+def read_file():
+    """Вернуть список тегов"""
 
-if _rF is not None:
-    tags = []
-    with open (f'{_rF}\\index.html') as f:
-        for line1 in range(1):
-            next(f)
-        for line in f:
-            tag1 = line.split()
-            if tag1:
-                tag2 = tag1[0].split('>')
-                tag = tag2[0]
-            if (tag[0] == '<') and (tag[1] != '/') and (tag[1:] not in tags):
-                tags.append(tag[1:])
-        print ("Список тегов:")
-        for i in tags:
-            print (i, end=' ')
-else:
-    print ("Файл отсутствует!")
+    tags = []  # возвращаемый список переменных
+    err_event = None  # возвращаемый текст ошибки
+    _sl = ["practice1", "Git"]  # Список корневых папок
+    t = os.getcwd()
+    _rF = None  # путь к обрабатываему файлу
+    for item in _sl:
+        _wd = f'{t[:t.rfind(item)+len(item)]}\\HTML'
+        # Проверка существования файла
+        if os.path.isfile(f'{_wd}\\index.html'):
+            _rF = _wd
+            break
+    if _rF is not None:
+        
+        with open (f'{_rF}\\index.html') as f:
+            for line1 in range(1):
+                next(f)
+            for line in f:
+                tag1 = line.split()
+                if tag1:
+                    tag2 = tag1[0].split('>')
+                    tag = tag2[0]
+                if (tag[0] == '<') and (tag[1] != '/') and (tag[1:] not in tags):
+                    tags.append(tag[1:])
+    else:
+        err_event = "Файл отсутствует!"
+    return tags, err_event
+
+
+if __name__ == "__main__":
+    tags, err_event = read_file()
+    if err_event is None:
+        # получен список тегов
+        [print(x.upper()) for x in tags]
+    else:
+       # ошибка формирования списка
+        print(err_event)
+
