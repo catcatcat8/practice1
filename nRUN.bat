22dc4b5636e32cca20cb9eca27b888a12e942be2
MirBoss <mirboss@gmail.com>
2020-07-15 14:58:56 +0300

    Create nRUN.bat

---
+++ nRUN.bat
@@ -0,0 +1,4 @@
+echo start Util Compress
+D:\MySystem\PythonEnvs381\python.exe -O htmltags.py D:\eDisk\ExsamServer\Script\Project\ExS37\HTML\templates D:\eDisk\ExsamServer\Script\Project\ExS37\HTML\css
+
+
